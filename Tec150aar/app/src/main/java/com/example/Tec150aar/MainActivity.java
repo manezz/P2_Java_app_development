5cdaec865feda8bbf803ef84cd87c2c9f7fd3b45
Magnus mano. Norling <mano@pc-tec.local>
2021-09-07 09:19:05 +0200

    first commit

---
+++ Tec150aar/app/src/main/java/com/example/Tec150aar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.Tec150aar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.tec150aar.dk");
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        }
+        super.onBackPressed();
+    }
+}
